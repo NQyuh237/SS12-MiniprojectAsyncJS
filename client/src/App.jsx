780226188e20fbe8fe35e472ae6e2d273ca576a3
nguyen huy <quanghuy2371998@gmail.com>
2023-09-13 07:34:59 +0700

    up

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import Loading from './components/base/loading/Loading'
+import ListUser from './pages/admin/ListUser'
+import ListProduct from './pages/product/ListProduct'
+import Login from './pages/user/login/Login'
+import Register from "./pages/user/register/Register";
+
+function App() {
+
+  return (
+    <>
+      {/* <ListProduct /> */}
+      {/* <ListUser /> */}
+
+      {/* <Loading /> */}
+      {/* <Login /> */}
+      <Register />
+    </>
+  )
+}
+
+export default App
